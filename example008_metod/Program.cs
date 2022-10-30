256e41894761fe9de17f0a06048ba689ced5eae2
Nadin <icemylady@gmail.com>
2022-10-30 14:50:33 +0300

    example007/example008

---
+++ example008_metod/Program.cs
@@ -0,0 +1,35 @@
+﻿//int Max(int arg1, int arg2, int arg3);
+//{
+//    int result = arg1;
+//    if(arg2> result) result = arg2;
+//    if(arg3> result) result = arg3;
+//    return result;
+//}
+
+
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 13;
+int b2 = 21233;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 323;
+
+int max = a1;
+
+if (b1 > max ) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
