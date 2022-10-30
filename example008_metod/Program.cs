7659c6256fdb8d84166c80d32ea05177197262fb
Nadin <icemylady@gmail.com>
2022-10-30 15:10:32 +0300

    example008 v3

--- example008_metod/Program.cs
+++ example008_metod/Program.cs
@@ -17,10 +17,7 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 323;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+int max = Max (Max(a1, b1, c1),Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.Write("max = ");
 Console.WriteLine(max);
